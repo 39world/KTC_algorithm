7aed862c6332f7edc0ea12066279453211268197
Sanggu <rkdtkdrn93@naver.com>
2023-01-18 14:31:19 +0900

    [Programmers] 미로 탈출 명령어
    2023.01.18 스터디 숙제
    카카오 Lv.3 어렵다/..

--- SangGu/미로 탈출 명령어.py
+++ SangGu/미로 탈출 명령어.py
@@ -1,49 +1,32 @@
-## 메모리 문제가 있는 것 같다.
-def whereIsExit(position,command):
-    global answer, limitCnt, limitX, limitY, moving, target
-    
-    for dx,dy,commandInput in moving:
-        
-        if len(answer) != 0:
-            return
-        
-        if len(command) == limitCnt:  
-            if position == target:
-                answer.append(command)
-            return
-        
-        resource = limitCnt-len(command)
-        shortCut = abs(position[0]-target[0])+abs(position[1]-target[1])
-        
-        if resource < shortCut :
-            return
-        
-        x = position[0]+dx
-        y = position[1]+dy                    
-        
-        if x < 1 or x > limitX:
-            continue
-        if y < 1 or y > limitY:
-            continue
-        
-        whereIsExit([x,y],command+commandInput)
-    return   
-
 def solution(n, m, x, y, r, c, k):
     global answer, limitCnt, limitX, limitY, moving, target
-        
-    limitCnt = k
-    limitX = n
-    limitY = m
+
     target = [r,c]
-    answer = []
-    position = [x,y]
-    moving = [[1,0,'d'],[0,-1,'l'],[0,1,'r'],[-1,0,'u']]
-    command = ''
-    whereIsExit(position,command)
+    answer = 'impossible'
+    moving = [[-1,0,'u'],[0,1,'r'],[0,-1,'l'],[1,0,'d']]
+    stack = [[x,y,'']]
 
-    if len(answer) == 0:
-        return 'impossible'
-    
-    return answer[0]
+    while stack:
+        positionX,positionY,command = stack.pop()
+        if len(command) == k and [positionX,positionY] == target :
+                answer = command
+                break
 
+        resource = k-len(command)
+        shortCut = abs(positionX-r)+abs(positionY-c)
+
+        if resource < shortCut or resource %2 != shortCut%2:
+            continue
+
+        for dx,dy,commandInput in moving:
+            nextX = positionX+dx
+            nextY = positionY+dy
+
+            if nextX < 1 or nextX > n:
+                continue
+            if nextY < 1 or nextY > m:
+                continue
+
+            stack.append([nextX,nextY,command+commandInput])    
+
+    return answer
